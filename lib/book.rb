2a9336558c968847aecc0bfe2840089a9ff05136
Paul Jung <pbnjcub@gmail.com>
2023-02-10 05:36:00 -0500

    commit tree 9905eb20c91c299bec5d0ec3821be42c27971039

--- lib/book.rb
+++ lib/book.rb
@@ -1,16 +1,59 @@
 class Book
-    attr_accessor :author
-    attr_accessor :title
-    attr_accessor :page_count
-    attr_accessor :genre
+    attr_accessor :title, :author, :page_count, :genre
 
     def initialize(title)
         @title = title
+        @author = author
+        @page_count = page_count
+        @genre = genre
     end
-
+    
     def turn_page
         puts "Flipping the page...wow, you read fast!"
     end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Book
+#     attr_accessor :author
+#     attr_accessor :title
+#     attr_accessor :page_count
+#     attr_accessor :genre
+
+#     def initialize(title)
+#         @title = title
+#     end
+
+#     def turn_page
+#         puts "Flipping the page...wow, you read fast!"
+#     end
+# end
 
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,13 +1,12 @@
-# Make your shoe class here!
 class Shoe
-    attr_accessor :brand
-    attr_accessor :color
-    attr_accessor :size
-    attr_accessor :material
-    attr_accessor :condition
+    attr_accessor :brand, :color, :size, :material, :condition
 
     def initialize(brand)
         @brand = brand
+        @color = color
+        @size = size
+        @material = material
+        @condition = condition
     end
 
     def cobble
@@ -15,5 +14,46 @@ class Shoe
         @condition = "new"
     end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # Make your shoe class here!
+# class Shoe
+#     attr_accessor :brand
+#     attr_accessor :color
+#     attr_accessor :size
+#     attr_accessor :material
+#     attr_accessor :condition
+
+#     def initialize(brand)
+#         @brand = brand
+#     end
+
+#     def cobble
+#         puts "Your shoe is as good as new!"
+#         @condition = "new"
+#     end
+# end
 
 
